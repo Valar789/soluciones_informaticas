7e4b83951db5806bfc6fb9d144c7013c9f758665
Valar789 <luismzo789@gmail.com>
2022-10-18 16:58:46 -0500

    fixed 3

--- components/landing/ContactCard.js
+++ components/landing/ContactCard.js
@@ -7,7 +7,7 @@ import Image from 'next/image';
 export default function ContactCard({ img, title, children }) {
     return (
         <div className="w-full lg:w-3/12 px-4 text-center">
-            <div className="text-blue-gray-800 p-3 mx-4 shadow-lg rounded-full bg-white inline-flex items-center justify-center mb-6">
+            <div className="text-blue-gray-800 p-3 mx-4 rounded-lg bg-white inline-flex items-center justify-center mb-6">
                 <Image className='rounded-md' src={img} alt='logo' raised='true' />
             </div>
             <H6 color="gray">{title}</H6>
